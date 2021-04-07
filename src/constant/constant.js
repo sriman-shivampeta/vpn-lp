9f3a25690b5bc89ed3b294a8f11dbb6daf4460cc
Shivampeta <sriman.shivampeta@capgemini.com>
2021-04-07 17:28:39 +0530

    added store to fetch the nodeapi data

---
+++ src/constant/constant.js
@@ -0,0 +1,4 @@
+module.exports = {
+  localUrl: "http://localhost:5000/posts/",
+  herokuUrl: "https://sriman-nodeapi.herokuapp.com/posts/",
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,39 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import { localUrl, herokuUrl } from "@/constant/constant";
 
 Vue.use(Vuex);
 
+const baseUrl = document.baseURI.match("localhost") ? localUrl : herokuUrl;
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    posts: [],
+    error_message: "",
+  },
+  mutations: {
+    SET_STATE(state, message) {
+      state[message.stateName] = message.payload;
+    },
+  },
+  actions: {
+    async fetchPostsAction({ commit }) {
+      try {
+        const response = await axios.get(baseUrl);
+        if (response.status) {
+          commit("SET_STATE", {
+            stateName: "posts",
+            payload: response.data.data,
+          });
+        }
+      } catch (error) {
+        commit("SET_STATE", {
+          stateName: "error_message",
+          payload: error.message,
+        });
+      }
+    },
+  },
   modules: {},
 });
